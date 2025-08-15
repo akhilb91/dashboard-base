3441d0c5f84685319d74482ab24d36527bded989
Akhil Balakrishnan <akhil.balakrishnan@cloud.com>
2025-08-15 13:32:40 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="topbar">
+        <h1 className="topbar-title">Dashboard</h1>
+      </div>
+
+      <div className="content">
+       <h3>This is where you should add the panel</h3>
+      </div>
+    </div>
+  );
+};
+
+export default App;
